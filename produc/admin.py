4942c3d8d347afa40ad1b28fd06b400b4dc62c97
Aleksander <sashabagdasarov@gmail.com>
2020-05-24 13:13:40 +0300

    commit

--- produc/admin.py
+++ produc/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import products
+from .models import products, Question
 
 
 admin.site.register(products)
+admin.site.register(Question)
--- produc/views.py
+++ produc/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Choice, Question
+from django.urls import reverse
 
 def index(request):
-    return HttpResponse('Hello, world. you`re in produc index.')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list' : latest_question_list,
+    }
+    return render(request, 'produc/index.html',context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'produc/detail.html', {'question':question})
+
+def result(request, question_id):
+    
+    response = 'You are looking at the results of questions %s'
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        select_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'produc/detail.html', {
+            'question' : question,
+            'error_message' : 'You did not select a choice',
+        })       
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        
+        return HttpResponseRedirect(reverse('produc:results', args=(question_id,)))
